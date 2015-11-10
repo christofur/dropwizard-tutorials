6544f7d4ce841ae673043af01ca2de9bde5d8f58
Chris Perks <pop.chris@gmail.com>
2015-11-10 15:25:47 +0000

    Added first Hello, World tutorial files

---
+++ src/main/java/com/example/helloworld/resources/HelloMarsResource.java
@@ -0,0 +1,33 @@
+package com.example.helloworld.resources;
+
+import com.example.helloworld.api.Saying;
+import com.google.common.base.Optional;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Path("/hello-mars")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloMarsResource {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloMarsResource(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        final String value = String.format(template, name.or(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+}
